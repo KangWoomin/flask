c09e67a9817995a2c65486632332c9062fe91f03
KangWoomin <blacksu25@gmail.com>
2024-09-23 18:26:16 +0900

    database_update

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template, Response, request,redirect,url_for
-
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 def upload_video():
+    # create_db()
     return render_template('home.html')
+
+# def create_db():
+#     conn = sqlite3.connect('database.db')
+
+#     conn.execute("""CREATE TABLE IF NOT EXISTS videos(
+#                  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+#                  original_video_path TEXT, 
+#                  annotated_video_path TEXT, 
+#                  uploaded_at TIMESTEMP DEFAULT CURRENT_TIMESTAMP);
+#                  """)
+#     conn.commit()
+#     conn.close()
+
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 
@@ -25,19 +45,17 @@ def generate_frames(video_path, outpu_path):
     out = cv2.VideoWriter(outpu_path, fourcc, fps, (width, height))
     
     if not cap.isOpened():
-      return redirect(url_for('upload_video'))
+        return 
     
     while cap.isOpened():
         ret, frame = cap.read()
 
         if not ret:
-            return redirect(url_for('upload_video'))
-            
+            break
 
         results = model(frame)
 
         if results:
-
             result = results[0]
             annotated_frame = result.plot()
             out.write(annotated_frame)
@@ -46,13 +64,18 @@ def generate_frames(video_path, outpu_path):
             frame = buffer.tobytes()            
 
             yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n')
+
     cap.release()
     out.release()
     cv2.destroyAllWindows()
-    return redirect(url_for('upload_video'))
+
+    # 영상 끝날 때 종료 신호 전송
+    yield (b'--frame\r\n'
+           b'Content-Type: text/plain\r\n\r\nEND\r\n')
 
 
+from datetime import datetime
 @app.route('/annotated_video/', methods=['POST'])
 def annotated_video():
 
@@ -67,10 +90,15 @@ def annotated_video():
     video_path = os.path.join('video', video_name)
     video_file.save(video_path)
 
-    output_path = os.path.join('video',f'output_{video_name}')
-    #영상 끝나고 나서 다시 upload_video.html으로 돌아가게 구현 필요
-    return Response(generate_frames(video_path, output_path), mimetype='multipart/x-mixed-replace; boundary=frame',
-                    )
+    output_path = os.path.join('video/annotated_video',f'output_{video_name}_{datetime.now()}')
+
+    conn = get_db_connection()
+    conn.execute("INSERT INTO videos (original_video_path, annotated_video_path) VALUES(?,?)",
+                 (video_path, output_path))
+    conn.commit()
+    conn.close()
+    
+    return Response(generate_frames(video_path, output_path), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 
